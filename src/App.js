200cc3141da2def2cb369e691dab01282f2a6eb8
Ruben <64035906+rubenkretek@users.noreply.github.com>
2020-07-28 16:49:42 +0100

    Moved remaining pages into the 'pages' folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Home } from "./Home";
+import { Home } from "./pages/Home";
 import { InstaMinis } from "./pages/InstaMinis";
-import { Contact } from "./Contact";
+import { Contact } from "./pages/Contact";
 import { NoMatch } from "./NoMatch";
 import { Layout } from "./components/Layout";
 import { NavigationBar } from "./components/NavigationBar";
